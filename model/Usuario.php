c543e52f70511c13c63af64ff425e3aecce841e4
Davi Milioli <davimilioli2108@gmail.com>
2023-09-19 22:22:08 -0300

    criação modelo de usuario e usuario sql

---
+++ model/Usuario.php
@@ -0,0 +1,147 @@
+<?php
+
+// Tudo que for relacionado ao usuario, irá mexer nessa classe
+class Usuario
+{
+    private $id;
+    private $nome;
+    private $nascimento;
+    private $cpf;
+    private $nomeMaterno;
+    private $email;
+    private $sexo;
+    private $celular;
+    private $telefone;
+    private $login;
+    private $senha;
+
+    public function setarId(int $id)
+    {
+
+        $this->id = trim($id);
+    }
+
+    public function pegarId()
+    {
+        return $this->id;
+    }
+
+    public function setarNome($nome)
+    {
+
+        $this->nome = ucwords(trim($nome));
+    }
+
+    public function pegarNome()
+    {
+        return $this->nome;
+    }
+
+    public function setarNascimento($nascimento)
+    {
+        $dataFormatada = date("Y-m-d", strtotime(str_replace("/", "-", $nascimento)));
+        $this->nascimento = trim($dataFormatada);
+    }
+
+    public function pegarNascimento()
+    {
+        return $this->nascimento;
+    }
+
+    public function setarCpf($cpf)
+    {
+
+        $cpf = preg_replace('/[^0-9]/', '', $cpf);
+        $this->cpf = $cpf;
+    }
+
+    public function pegarCpf()
+    {
+        return $this->cpf;
+    }
+
+    public function setarNomeMaterno($nomeMaterno)
+    {
+        $this->nomeMaterno = $nomeMaterno;
+    }
+
+    public function pegarNomeMaterno()
+    {
+        return $this->nomeMaterno;
+    }
+
+    public function setarEmail($email)
+    {
+        $this->email = trim($email);
+    }
+
+    public function pegarEmail()
+    {
+        return $this->email;
+    }
+
+    public function setarSexo($sexo)
+    {
+
+        $this->sexo = $sexo;
+    }
+
+    public function pegarSexo()
+    {
+        return $this->sexo;
+    }
+
+    public function setarCelular($celular)
+    {
+
+        $celular = preg_replace('/[^0-9]/', '', $celular);
+        $this->celular = $celular;
+    }
+
+    public function pegarCelular()
+    {
+        return $this->celular;
+    }
+
+    public function setarTelefone($telefone)
+    {
+        $telefone = preg_replace('/[^0-9]/', '', $telefone);
+        $this->telefone = $telefone;
+    }
+
+    public function pegarTelefone()
+    {
+        return $this->telefone;
+    }
+
+    public function setarLogin($login)
+    {
+        $this->login = trim($login);
+    }
+
+    public function pegarLogin()
+    {
+        return $this->login;
+    }
+
+    public function setarSenha($senha)
+    {
+        $this->senha = trim($senha);
+    }
+
+    public function pegarSenha()
+    {
+        return $this->senha;
+    }
+}
+
+interface UsuarioSqlInterface
+{
+    public function criarUsuario(Usuario $usuario);
+    public function consultarEmail($email);
+    public function consultarDados();
+}
+
+/* $login = new Login;
+$login->setarSexo('masculino');
+echo $login->pegarSexo(); */
---
+++ modelSql/UsuarioMySql.php
@@ -0,0 +1,78 @@
+<?php
+require_once(__DIR__ . '/../model/Usuario.php');
+
+class UsuarioMySql implements UsuarioSqlInterface
+{
+    private $pdo;
+
+    // IMPLEMENTAÇÃO QUE USA O DRIVER DO PDO
+    public function __construct(PDO $driver)
+    {
+        $this->pdo = $driver;
+    }
+
+    // CRIAR USUARIO
+    public function criarUsuario(Usuario $usuario)
+    {
+        $sql = $this->pdo->prepare("INSERT INTO usuarios (nome, nascimento, cpf, nomematerno, email, sexo, celular, telefone, login, senha) VALUES (:nome, :nascimento, :cpf, :nomematerno, :email, :sexo, :celular, :telefone, :login, :senha)");
+        $sql->bindValue(':nome', $usuario->pegarNome());
+        $sql->bindValue(':nascimento', $usuario->pegarNascimento());
+        $sql->bindValue(':cpf', $usuario->pegarCpf());
+        $sql->bindValue(':nomematerno', $usuario->pegarNomeMaterno());
+        $sql->bindValue(':email', $usuario->pegarEmail());
+        $sql->bindValue(':sexo', $usuario->pegarSexo());
+        $sql->bindValue(':celular', $usuario->pegarCelular());
+        $sql->bindValue(':telefone', $usuario->pegarTelefone());
+        $sql->bindValue(':login', $usuario->pegarLogin());
+        $sql->bindValue(':senha', $usuario->pegarSenha());
+        $sql->execute();
+
+        $usuario->setarId($this->pdo->lastInsertId());
+
+        return $usuario;
+    }
+
+    // CONSULTAR EMAIL
+    public function consultarEmail($email)
+    {
+        $sql = $this->pdo->prepare("SELECT * FROM usuarios WHERE email = :email");
+        $sql->bindValue(':email', $email);
+        $sql->execute();
+
+
+        if ($sql->rowCount() > 0) {
+            $data = $sql->fetch(PDO::FETCH_ASSOC);
+            $usuario = new Usuario();
+            $usuario->setarId($data['id']);
+            $usuario->setarNome($data['nome']);
+            $usuario->setarNascimento($data['nascimento']);
+            $usuario->setarCpf($data['cpf']);
+            $usuario->setarNomeMaterno($data['nomematerno']);
+            $usuario->setarEmail($data['email']);
+            $usuario->setarSexo($data['sexo']);
+            $usuario->setarCelular($data['celular']);
+            $usuario->setarTelefone($data['telefone']);
+            $usuario->setarLogin($data['login']);
+            $usuario->setarSenha($data['senha']);
+            return $usuario;
+        } else {
+            return false;
+        }
+    }
+
+    public function consultarDados()
+    {
+        $array = [];
+
+        $sql = $this->pdo->prepare("SELECT * FROM usuarios");
+        $sql->execute();
+        if ($sql->rowCount() > 0) {
+            $data = $sql->fetchAll(PDO::FETCH_ASSOC);
+            foreach ($data as $item) {
+                $array[] = $item;
+            }
+        }
+
+        return $array;
+    }
+}
