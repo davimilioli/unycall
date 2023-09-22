dff0960ed236f9543eb819ef8f44b1219ab9650d
Davi Milioli <davimilioli2108@gmail.com>
2023-09-21 22:57:06 -0300

    layout e itens header

--- includes/header.php
+++ includes/header.php
@@ -1,1 +1,22 @@
-<a href="../pages/cadastro/cadastro.php">Cadastro</a>
+<header class="header">
+    <div class="logo">
+        <a href="/index.php"><img src="/assets/img/logo.png" alt="Logo UnyCall"></a>
+    </div>
+    <nav class="menu">
+        <ul class="menu-list">
+            <li class="menu-list-item"><a href="#">Página Inicial</a></li>
+            <li class="menu-list-item"><a href="#">Sobre</a></li>
+            <li class="menu-list-item"><a href="#">Serviços</a></li>
+            <li class="menu-list-item"><a href="#">Contato</a></li>
+        </ul>
+    </nav>
+    <div class="header-actions">
+        <a class="btn" href="/pages/cadastro/cadastro.php">Cadastrar-se</a>
+        <a class="btn secondary" href="/pages/login/login.php">Login</a>
+    </div>
+    <button type="button" class="menu-mobile">
+        <span></span>
+        <span></span>
+        <span></span>
+    </button>
+</header>
