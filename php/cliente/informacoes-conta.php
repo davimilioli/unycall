6379891ab336287f06112307d82e916b21de0317
Davi Milioli <davimilioli2108@gmail.com>
2023-10-08 18:06:47 -0300

    dropdown de perfil e página informações de perfil

---
+++ php/cliente/informacoes-conta.php
@@ -0,0 +1,39 @@
+<?php
+require_once(__DIR__ . '/Sistema.php');
+$sistema = new Sistema($pdo);
+
+$verificarPerm = $sistema->procurarIdUsuario($_GET['id']);
+if ($verificarPerm['usuario']['permissao'] == 'administrador') {
+    session_name('administrador');
+} else {
+    session_name('usuario');
+}
+
+session_start();
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="../../assets/img/favicon.ico" type="image/x-icon">
+    <title>Unycall - Editor de Usuário</title>
+    <link rel="stylesheet" href="../../assets/css/css/style.css">
+</head>
+
+<body>
+    <?php require_once(__DIR__ . '/layout/includes/header.php'); ?>
+    <div class="page-cliente">
+        <?php require_once(__DIR__ . '/layout/includes/aside.php'); ?>
+        <main>
+
+        </main>
+    </div>
+    <script src="../../assets/js/cliente.js"></script>
+    <script src="../../assets/js/lista-usuarios.js"></script>
+</body>
+
+</html>
--- php/cliente/layout/includes/header.php
+++ php/cliente/layout/includes/header.php
@@ -1,3 +1,10 @@
+<?php
+require_once(__DIR__ . '../../../Sistema.php');
+$sistema = new Sistema($pdo);
+
+$usuario = $sistema->procurarIdUsuario($_GET['id']);
+?>
+
 <header class="header cliente">
     <button class="aside-open">
         <img src="/assets/img/icons/menu-open.svg">
@@ -14,13 +21,40 @@
             <li class="menu-list-item"><a href="#">Serviços</a></li>
             <li class="menu-list-item"><a href="#">Contato</a></li>
         </ul>
-        <div class="header-actions">
-            <a class="btn" href="sair.php">Sair</a>
-        </div>
     </nav>
     <button type="button" class="menu-mobile">
         <span></span>
         <span></span>
         <span></span>
     </button>
+    <div class="menu-profile">
+        <button class="menu-profile-button">
+            <img src="/assets/img/icons/profile.svg">
+        </button>
+        <div class="menu-profile-content">
+            <div class="menu-profile-header">
+                <div class="menu-profile-header-name"><?= $usuario['usuario']['nome'] ?></div>
+                <div class="menu-profile-header-email"><?= strlen($usuario['usuario']['email']) > 30 ? substr($usuario['usuario']['email'], 0, 30) . '...' : $usuario['usuario']['email'] ?></div>
+            </div>
+            <div class="menu-profile-body">
+                <ul class="menu-profile-body-list">
+                    <li class="menu-profile-body-title"><a href="./informacoes-conta.php?id=<?= $_GET['id'] ?>">Informações da conta</a></li>
+                    <li class="menu-profile-body-title">Segurança</li>
+                    <li class="menu-profile-body-title">Atividades de Conta</li>
+                    <li class="menu-profile-body-title">Suporte</li>
+                </ul>
+            </div>
+            <div class="menu-profile-footer">
+                <a class="btn" href="sair.php">Sair</a>
+            </div>
+        </div>
+    </div>
 </header>
+
+<script>
+    const openMenuProfile = document.querySelector('.menu-profile-button');
+    const menuProfile = document.querySelector('.menu-profile-content');
+    openMenuProfile.addEventListener('click', () => {
+        menuProfile.classList.toggle('active')
+    })
+</script>
